e0d93f61d6102e574608c28baaaed1fdec56cc89
sultan-kanaan <sultan.kanaan92@gmail.com>
2022-02-13 12:06:36 +0200

    task 5

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ var name = prompt("What is your name ?");
 var gender = prompt("Are you male or female ?");
 var Age = prompt("How old are you ?");
 var age = Number(Age);
-confirm("Do you want to skip ? ");
 
-if(age <= 0 ){
-alert("your age is wrong !");
-}
 
-if(gender === "male" || gender === "Male"){
+
+var skip = confirm("Do you want to skip ? ");
+if  (skip == true){
+      console.log(skip);           
+     }
+else{
+
+    if(gender === "male" || gender === "Male"){
 alert(`welcome Mr. ${name}`);
 }
 else  if(gender === "female"||gender === "Feale"){
 alert(`welcome Ms. ${name}`);
 }
 else{
-alert(`welcome  ${name});
+alert(`welcome  ${name}`);
 }
+   
+   }
 
+   if(age <= 0 ){
+    alert("your age is wrong !");
+    }
 
